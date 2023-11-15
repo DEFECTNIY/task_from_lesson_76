bcfb19dafb94f1bc1f28ff927bc0ca2ed2c10c74
DEFECTNIY <145113063+DEFECTNIY@users.noreply.github.com>
2023-11-15 20:15:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Albert'
+const secondName = 'Akhmetov'
+const thirdName = 'Uralovich'
+let city = 'UFA'
+let age = '21'
+
+console.log(firstName,secondName, thirdName, city, age,'y.o')
